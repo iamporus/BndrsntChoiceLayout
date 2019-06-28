88caea376821ff0b504fe1ccb14eb810ee49d6e3
iamporus <iamporus@gmail.com>
2019-06-28 13:50:54 +0530

    added default and selected choice color support through XML attributes.

--- bndrsntchoicelayout/src/main/java/com/prush/bndrsntchoicelayout/BndrsntChoiceLayout.kt
+++ bndrsntchoicelayout/src/main/java/com/prush/bndrsntchoicelayout/BndrsntChoiceLayout.kt
@@ -3,6 +3,8 @@ package com.prush.bndrsntchoicelayout
 import android.animation.ObjectAnimator
 import android.arch.lifecycle.LifecycleObserver
 import android.content.Context
+import android.support.annotation.ColorInt
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
@@ -11,11 +13,23 @@ import kotlinx.android.synthetic.main.choice_layout.view.*
 
 class BndrsntChoiceLayout : RelativeLayout {
 
+    companion object {
+
+        private const val DEFAULT_CHOICE_COLOR = android.R.color.darker_gray
+        private const val SELECTED_CHOICE_COLOR = android.R.color.white
+    }
+
     private var numberOfChoices: Int = 0
     private var choiceOneText: String? = null
     private var choiceTwoText: String? = null
     private var choiceThreeText: String? = null
     private var bRevealMode: Boolean = false
+
+    @ColorInt
+    private var defaultChoiceColor: Int = 0
+
+    @ColorInt
+    private var selectedChoiceColor: Int = 0
 
     private var onChoiceSelectedListener: OnChoiceSelectedListener? = null
 
@@ -58,6 +72,19 @@ class BndrsntChoiceLayout : RelativeLayout {
             choiceTwoText = typedArray.getString(R.styleable.BndrsntChoiceLayout_choice_two_text)
             choiceThreeText = typedArray.getString(R.styleable.BndrsntChoiceLayout_choice_three_text)
 
+            defaultChoiceColor =
+                typedArray.getColor(R.styleable.BndrsntChoiceLayout_default_choice_color, 0)
+
+            if (defaultChoiceColor == 0) {
+                defaultChoiceColor = ContextCompat.getColor(context, DEFAULT_CHOICE_COLOR)
+            }
+
+            selectedChoiceColor =
+                typedArray.getColor(R.styleable.BndrsntChoiceLayout_selected_choice_color, 0)
+
+            if (selectedChoiceColor == 0) {
+                selectedChoiceColor = ContextCompat.getColor(context, SELECTED_CHOICE_COLOR)
+            }
             bRevealMode = typedArray.getBoolean(R.styleable.BndrsntChoiceLayout_reveal_mode, false)
 
             typedArray.recycle()
@@ -109,15 +136,31 @@ class BndrsntChoiceLayout : RelativeLayout {
 
             // keep the view hidden in the beginning
             alpha = if (bRevealMode) 0f else 1f
+
+            choiceOneTextView.setTextColor(defaultChoiceColor)
+            choiceTwoTextView.setTextColor(defaultChoiceColor)
+            choiceSingleTextView.setTextColor(defaultChoiceColor)
         }
     }
 
     private fun onChoiceSelected(id: Int, choiceText: String) {
 
         onChoiceSelectedListener?.onChoiceSelected(id, choiceText)
-        //stop progress
 
-        // change choice backgrounds
+        bndrSntchTimer.reset()
+
+        when(id)
+        {
+            R.id.choiceOneTextView -> {
+                choiceOneTextView.setTextColor(selectedChoiceColor)
+            }
+            R.id.choiceTwoTextView -> {
+                choiceTwoTextView.setTextColor(selectedChoiceColor)
+            }
+            R.id.choiceSingleTextView -> {
+                choiceSingleTextView.setTextColor(selectedChoiceColor)
+            }
+        }
 
         //cleanup
     }
