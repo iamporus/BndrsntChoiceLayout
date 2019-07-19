5b8de07b95fb579163e05b51f61e8bbee9eb8142
iamporus <iamporus@gmail.com>
2019-07-19 20:00:21 +0530

    Updated sample usage.

--- app/src/main/java/com/prush/bndrsntchoicelayoutsample/MainActivity.kt
+++ app/src/main/java/com/prush/bndrsntchoicelayoutsample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.prush.bndrsntchoicelayoutsample
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.prush.bndrsntchoicelayout.BndrsntChoiceLayout
@@ -11,6 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bndrnstChoiceLayout.numberOfChoices = 3
+        bndrnstChoiceLayout.choiceOneText = "Yes"
+        bndrnstChoiceLayout.choiceTwoText = "No"
+        bndrnstChoiceLayout.choiceThreeText = "Whatever"
+        bndrnstChoiceLayout.bRandomizeChoice = true
+        bndrnstChoiceLayout.bRevealMode = true
+        bndrnstChoiceLayout.defaultChoiceColor = ContextCompat.getColor(applicationContext, android.R.color.holo_blue_light)
 
         if (savedInstanceState == null) {
             bndrnstChoiceLayout.postDelayed({
@@ -23,7 +32,7 @@ class MainActivity : AppCompatActivity() {
 
             }, 2000)
         }
-        bndrnstChoiceLayout.setOnChoiceSelectedListener(object : BndrsntChoiceLayout.OnChoiceSelectedListener {
+        bndrnstChoiceLayout.onChoiceSelectedListener = (object : BndrsntChoiceLayout.OnChoiceSelectedListener {
             override fun onChoiceSelected(id: Int, choiceText: String) {
                 Log.d("MainActivity", "selected: $choiceText")
 
